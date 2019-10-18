f03383b6942e39333c70c286b7516403e562a2aa
Jen Lindner <jenlindner22@gmail.com>
2019-10-18 03:08:12 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -50,15 +50,9 @@ def naming_system
    }
     }
 {
-  :label => "Family",
-  :sub_category => {
-    :label => "Genus",
-  }
+  :label => "Species",
+  :sub_category => nil
 }
-# {
-#   :label => "Species",
-#   :sub_category => nil
-# }
   
  
  #   :kingdom => 
